aa823d8728a728823c02a4827a38cc21b0ebea89
Gregorson <gregorson0x15@proton.me>
2025-04-20 20:55:49 +0200

    Other files

---
+++ app/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, FlatList, StyleSheet, Text, StatusBar} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+import data from './book_order.json';
+
+
+type ItemProps = {title: string};
+
+const Item = ({title}: ItemProps) => (
+    <View style={styles.item}>
+        <Text style={styles.title}>{title}</Text>
+    </View>
+);
+
+const App = () => (
+    <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={data.DATA}
+                renderItem={({item}) => <Item title={item.title} />}
+               ></FlatList>
+        </SafeAreaView>
+    </SafeAreaProvider>
+);
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+});
+
+export default App;
