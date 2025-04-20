9af13e672c804b3309ff761ed944b9a634ad2fcc
Gregorson <gregorson0x15@proton.me>
2025-04-20 20:53:51 +0200

    List of Bible books

---
+++ app/book_data.tsx
@@ -0,0 +1,136 @@
+import Genesis from './KJV_Bible/Genesis.json';
+import Exodus from './KJV_Bible/Exodus.json';
+import Leviticus from './KJV_Bible/Leviticus.json';
+import Numbers from './KJV_Bible/Numbers.json';
+import Deuteronomy from './KJV_Bible/Deuteronomy.json';
+import Joshua from './KJV_Bible/Joshua.json';
+import Judges from './KJV_Bible/Judges.json';
+import Ruth from './KJV_Bible/Ruth.json';
+import FirstSamuel from './KJV_Bible/1Samuel.json';
+import SecondSamuel from './KJV_Bible/2Samuel.json';
+import FirstKings from './KJV_Bible/1Kings.json';
+import SecondKings from './KJV_Bible/2Kings.json';
+import FirstChronicles from './KJV_Bible/1Chronicles.json';
+import SecondChronicles from './KJV_Bible/2Chronicles.json';
+import Ezra from './KJV_Bible/Ezra.json';
+import Nehemiah from './KJV_Bible/Nehemiah.json';
+import Esther from './KJV_Bible/Esther.json';
+import Job from './KJV_Bible/Job.json';
+import Psalms from './KJV_Bible/Psalms.json';
+import Proverbs from './KJV_Bible/Proverbs.json';
+import Ecclesiastes from './KJV_Bible/Ecclesiastes.json';
+import SongOfSolomon from './KJV_Bible/SongofSolomon.json';
+import Isaiah from './KJV_Bible/Isaiah.json';
+import Jeremiah from './KJV_Bible/Jeremiah.json';
+import Lamentations from './KJV_Bible/Lamentations.json';
+import Ezekiel from './KJV_Bible/Ezekiel.json';
+import Daniel from './KJV_Bible/Daniel.json';
+import Hosea from './KJV_Bible/Hosea.json';
+import Joel from './KJV_Bible/Joel.json';
+import Amos from './KJV_Bible/Amos.json';
+import Obadiah from './KJV_Bible/Obadiah.json';
+import Jonah from './KJV_Bible/Jonah.json';
+import Micah from './KJV_Bible/Micah.json';
+import Nahum from './KJV_Bible/Nahum.json';
+import Habakkuk from './KJV_Bible/Habakkuk.json';
+import Zephaniah from './KJV_Bible/Zephaniah.json';
+import Haggai from './KJV_Bible/Haggai.json';
+import Zechariah from './KJV_Bible/Zechariah.json';
+import Malachi from './KJV_Bible/Malachi.json';
+import Matthew from './KJV_Bible/Matthew.json';
+import Mark from './KJV_Bible/Mark.json';
+import Luke from './KJV_Bible/Luke.json';
+import John from './KJV_Bible/John.json';
+import Acts from './KJV_Bible/Acts.json';
+import Romans from './KJV_Bible/Romans.json';
+import FirstCorinthians from './KJV_Bible/1Corinthians.json';
+import SecondCorinthians from './KJV_Bible/2Corinthians.json';
+import Galatians from './KJV_Bible/Galatians.json';
+import Ephesians from './KJV_Bible/Ephesians.json';
+import Philippians from './KJV_Bible/Philippians.json';
+import Colossians from './KJV_Bible/Colossians.json';
+import FirstThessalonians from './KJV_Bible/1Thessalonians.json';
+import SecondThessalonians from './KJV_Bible/2Thessalonians.json';
+import FirstTimothy from './KJV_Bible/1Timothy.json';
+import SecondTimothy from './KJV_Bible/2Timothy.json';
+import Titus from './KJV_Bible/Titus.json';
+import Philemon from './KJV_Bible/Philemon.json';
+import Hebrews from './KJV_Bible/Hebrews.json';
+import James from './KJV_Bible/James.json';
+import FirstPeter from './KJV_Bible/1Peter.json';
+import SecondPeter from './KJV_Bible/2Peter.json';
+import FirstJohn from './KJV_Bible/1John.json';
+import SecondJohn from './KJV_Bible/2John.json';
+import ThirdJohn from './KJV_Bible/3John.json';
+import Jude from './KJV_Bible/Jude.json';
+import Revelation from './KJV_Bible/Revelation.json';
+
+export default {
+    Genesis,
+    Exodus,
+    Leviticus,
+    Numbers,
+    Deuteronomy,
+    Joshua,
+    Judges,
+    Ruth,
+    FirstSamuel,
+    SecondSamuel,
+    FirstKings,
+    SecondKings,
+    FirstChronicles,
+    SecondChronicles,
+    Ezra,
+    Nehemiah,
+    Esther,
+    Job,
+    Psalms,
+    Proverbs,
+    Ecclesiastes,
+    SongOfSolomon,
+    Isaiah,
+    Jeremiah,
+    Lamentations,
+    Ezekiel,
+    Daniel,
+    Hosea,
+    Joel,
+    Amos,
+    Obadiah,
+    Jonah,
+    Micah,
+    Nahum,
+    Habakkuk,
+    Zephaniah,
+    Haggai,
+    Zechariah,
+    Malachi,
+    Matthew,
+    Mark,
+    Luke,
+    John,
+    Acts,
+    Romans,
+    FirstCorinthians,
+    SecondCorinthians,
+    Galatians,
+    Ephesians,
+    Philippians,
+    Colossians,
+    FirstThessalonians,
+    SecondThessalonians,
+    FirstTimothy,
+    SecondTimothy,
+    Titus,
+    Philemon,
+    Hebrews,
+    James,
+    FirstPeter,
+    SecondPeter,
+    FirstJohn,
+    SecondJohn,
+    ThirdJohn,
+    Jude,
+    Revelation,
+
+};
